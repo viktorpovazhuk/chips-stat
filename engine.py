34eff0edcb3a0dd222a517ba8f77d11bd52e0137
vityha <dpyg.ttpocto@gmail.com>
2021-04-12 17:50:38 +0300

    First files

---
+++ engine.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def read_root():
+    """
+    Return content of root html file
+    """
+    if request.method == "GET":
+        return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
